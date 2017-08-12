4b433fdf05029e3babb83cec357e5e6942fffc4f
kutyel <flaviocorpa@gmail.com>
2017-08-12 11:41:51 +0200

    Implement the form to add stores

--- controllers/store.js
+++ controllers/store.js
@@ -1,2 +1,8 @@
 exports.homePage = (req, res) =>
   res.render('index', req.query)
+
+exports.addStore = (req, res) =>
+  res.render('edit', { title: 'Add Store' })
+
+exports.createStore = (req, res) =>
+  res.json(req.body)
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const storeCtrl = require('../controllers/store')
 
 router.get('/', storeCtrl.homePage)
+router.get('/add', storeCtrl.addStore)
+router.post('/add', storeCtrl.createStore)
 
 module.exports = router
