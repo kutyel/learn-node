b11f434b259f78c6d9dac216afb733ea4ab1a988
kutyel <flaviocorpa@gmail.com>
2017-08-30 10:23:06 +0200

    Creating an AJAX search interface

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -2,5 +2,8 @@ import '../sass/style.scss'
 
 import { $, $$ } from './modules/bling'
 import autocomplete from './modules/autocomplete'
+import typeahead from './modules/typeahead'
 
 autocomplete($('#address'), $('#lat'), $('#lng'))
+
+typeahead($('.search'))
---
+++ public/javascripts/modules/typeahead.js
@@ -0,0 +1,64 @@
+import purify from 'dompurify'
+
+const resultsHTML = stores =>
+  stores
+    .map(
+      ({ slug, name }) => `
+  <a href="/store/${slug}" class="search__result">
+    <strong>${name}</strong>
+  </a>
+  `
+    )
+    .join('')
+
+export default search => {
+  if (search) {
+    const input = search.querySelector('input[name="search"]')
+    const results = search.querySelector('.search__results')
+
+    input.on('input', function() {
+      if (!this.value) {
+        results.style.display = 'none'
+        return
+      }
+      results.style.display = 'block'
+      results.innerHTML = ''
+      fetch(`/api/search?q=${this.value}`)
+        .then(res => res.json())
+        .then(
+          data =>
+            (results.innerHTML = purify.sanitize(
+              data.length
+                ? resultsHTML(data)
+                : `<div class="search__result">No results for <mark>${this
+                    .value}</mark> found!</div>`
+            ))
+        )
+        .catch(err => console.error(err))
+    })
+
+    // handle keyboard inputs
+    input.on('keyup', ({ keyCode }) => {
+      if ([38, 40, 13].includes(keyCode)) {
+        const active = 'search__result--active'
+        const current = search.querySelector(`.${active}`)
+        const items = search.querySelectorAll('.search__result')
+        let next
+        if (keyCode === 40 && current) {
+          next = current.nextElementSibling || items[0]
+        } else if (keyCode === 40) {
+          next = items[0]
+        } else if (keyCode === 38 && current) {
+          next = current.previousElementSibling || items[items.length - 1]
+        } else if (keyCode === 38) {
+          next = items[items.length - 1]
+        } else if (keyCode === 13 && current) {
+          window.location = current.href
+          return
+        }
+        current && current.classList.remove(active)
+        next.classList.add(active)
+      }
+    })
+  }
+}
